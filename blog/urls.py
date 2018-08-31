4614f76f0fecdbf3c8847318b7328a8c48462ba9
Nico Loffreda <nicolof88@gmail.com>
2018-08-30 19:59:11 -0700

    Blog List working. Added index.html

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+# Define the namespace of app to avoid name clashes
+app_name = 'blog'
+
+# URL Patterns for blog app
+urlpatterns = [
+    # Entry point : /blog/
+    path('', views.index, name="index"),
+]
